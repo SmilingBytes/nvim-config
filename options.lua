c0cac735c19b7469235198f3b5b3483a26dbc4ff
Ismail H Rana <rana@mirinfosys.com>
2022-12-14 20:20:03 +0600

    update fold method, level

--- options.lua
+++ options.lua
@@ -1,8 +1,14 @@
 local options = {
   swapfile = false,                        -- creates a swapfile
   colorcolumn = '90',
+  foldmethod = "expr",
+  foldexpr = "nvim_treesitter#foldexpr()",
+  foldnestmax = 10,
+  foldlevel = 9,
 }
 
+local opt = vim.opt
+
 for k, v in pairs(options) do
-  vim.opt[k] = v
+  opt[k] = v
 end
