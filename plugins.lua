b190c5d43cfe059f304c3237a1ce8603346c7b4d
Ismail H Rana <rana@mirinfosys.com>
2023-05-12 13:47:27 +0600

    update keymapping for codeium accept, next, prev

--- plugins.lua
+++ plugins.lua
@@ -129,7 +129,9 @@ local plugins = {
     event = { "InsertEnter" },
     config = function ()
       -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, noremap = true})
+      vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<c-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
     end
   },
